e3355e55bae3fdb0090037fa75892f5bf7d02f34
Zubair Akhtar <108027134+ZubairAkhtar123@users.noreply.github.com>
2025-07-12 21:50:29 +0500

    Update Sniffer.py

--- Sniffer.py
+++ Sniffer.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-from scapy.layers.inet import IP 
+from scapy.layers.inet import IP
 
 def process_packet(packet):
     if packet.haslayer(IP):
@@ -16,10 +16,18 @@ def process_packet(packet):
             try:
                 payload = packet[Raw].load.decode(errors='ignore')
                 print("Payload:\n", payload)
+
+                # Host header dhoondhna (HTTP request ka)
+                if "Host:" in payload:
+                    host_line = [line for line in payload.split('\r\n') if "Host:" in line]
+                    if host_line:
+                        website = host_line[0].split("Host: ")[1]
+                        print("[*] Website Visited:", website)
             except:
                 print("Payload could not be decoded.")
 
-#Interace name
-iface_name = "\\Device\\NPF_{A8B16C33-7E8E-4EBA-8C4D-3FE995669AA3}"  
+# Replace this with your own interface
+iface_name = "\\Device\\NPF_{A8B16C33-7E8E-4EBA-8C4D-3FE995669AA3}"
 
-sniff(filter="ip", iface=iface_name, prn=process_packet, store=0)
+# Only capture 10 packets
+sniff(filter="tcp port 80", iface=iface_name, prn=process_packet, store=0, count=10)
